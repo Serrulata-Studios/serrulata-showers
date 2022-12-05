c51bf9c5a2aa584f0779b97a9bd4e560c255ee48
TOSHKO <84236467+TOSHKO003@users.noreply.github.com>
2022-12-05 23:34:40 +0200

    update
    better progressbar,
    proper qb-target removal

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,14 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-
-local shower = false
+local targets = {}
+local showering = false
 
 CreateThread(function()
-    for i, v in pairs(Config.Locations) do
-        local coords = Config.Locations[i].coords
-        local spherename = Config.Locations[i].name
+    for i=1, #Config.Locations do
+        local shower = Config.Locations[i]
+        local coords = shower.coords
+        local spherename = ("Shower_%s"):format(i)
         if Config.Framework == 'QB' then
-            exports['qb-target']:AddCircleZone(spherename, vector3(coords.x, coords.y, coords.z), 0.6, {
+            targets[spherename] = exports['qb-target']:AddCircleZone(spherename, coords, 0.6, {
                 name = spherename,
                 useZ = true,
                 }, {
@@ -18,154 +19,132 @@ CreateThread(function()
                             icon = 'fas fa-shower',
                             label = 'Take Shower',
                             canInteract = function(entity, distance, coords, name)
-                                if shower == false and distance < 1 then
-                                    return true
-                                else
-                                    return false
-                                end
+                                return not showering and distance < 1
                             end
                         },
                     },
                 distance = 1.5
             })
         elseif Config.Framework == 'OX' then
-            local coords = Config.Locations[i].coords
             local radius = Config.Locations[i].radius
-            exports.ox_target:addSphereZone({ -- Enter
-            coords = vector3(coords.x, coords.y, coords.z),
-            radius = radius,
-            options = {
-                {
-                    name = 'sphere',
-                    icon = 'fas fa-shower',
-                    label = 'Take Shower',
-                    onSelect = function()
-                        TriggerEvent('serrulata-shower:client:takeShower')
-                    end,
-                    canInteract = function(entity, distance, coords, name)
-                        if shower == false and distance < 1 then
-                            return true
-                        else
-                            return false
+            exports.ox_target:addSphereZone({
+                coords = coords,
+                radius = radius,
+                options = {
+                    {
+                        name = spherename,
+                        icon = 'fas fa-shower',
+                        label = 'Take Shower',
+                        onSelect = function()
+                            TriggerEvent('serrulata-shower:client:takeShower')
+                        end,
+                        canInteract = function(entity, distance, coords, name)
+                            return not showering and distance < 1
                         end
-                    end
-                },
-            }
+                    },
+                }
             })
         end
     end
 end)
 RegisterNetEvent('serrulata-shower:client:takeShower', function()
-    local PlayerPed = plyPed
-    if GetEntityModel(plyPed) == -1667301416 then
+    local sex = 'male'
+    local PlayerPed = PlayerPedId()
+    if GetEntityModel(PlayerPed) == -1667301416 then
         sex = 'female'
     else
         sex = 'male'
     end
-    if sex == 'male' then
-        if shower == false then
-            shower = true
-            FreezeEntityPosition((PlayerPedId()), true)
-            local coords = GetEntityCoords(PlayerPedId())
-            maleProgressBar()
-            while shower == true do
-                if not HasNamedPtfxAssetLoaded("core") then
-                    RequestNamedPtfxAsset("core")
-                    while not HasNamedPtfxAssetLoaded("core") do
-                        Wait(1)
-                    end
+    if not showering then
+        showering = true
+        FreezeEntityPosition((PlayerPedId()), true)
+        local coords = GetEntityCoords(PlayerPedId())
+        ProgressBar(sex)
+        while showering do
+            if not HasNamedPtfxAssetLoaded("core") then
+                RequestNamedPtfxAsset("core")
+                while not HasNamedPtfxAssetLoaded("core") do
+                    Wait(1)
                 end
-                UseParticleFxAssetNextCall("core") 
-                particles  = StartParticleFxLoopedAtCoord("ent_sht_water", coords.x, coords.y, coords.z +1.2, 0.0, 0.0, 0.0, 1.0, false, false, false, false) 
-                UseParticleFxAssetNextCall("core") 
-                Wait(3000)
             end
-        end
-    elseif sex == 'female' then
-        if shower == false then
-            shower = true
-            FreezeEntityPosition((PlayerPedId()), true)
-            local coords = GetEntityCoords(PlayerPedId())
-            femaleProgressBar()
-            while shower == true do
-                if not HasNamedPtfxAssetLoaded("core") then
-                    RequestNamedPtfxAsset("core")
-                    while not HasNamedPtfxAssetLoaded("core") do
-                        Wait(1)
-                    end
-                end
-                UseParticleFxAssetNextCall("core") 
-                particles  = StartParticleFxLoopedAtCoord("ent_sht_water", coords.x, coords.y, coords.z +1.2, 0.0, 0.0, 0.0, 1.0, false, false, false, false) 
-                UseParticleFxAssetNextCall("core") 
-                Wait(3000)
-            end
+            UseParticleFxAssetNextCall("core")
+            particles  = StartParticleFxLoopedAtCoord("ent_sht_water", coords.x, coords.y, coords.z +1.2, 0.0, 0.0, 0.0, 1.0, false, false, false, false)
+            UseParticleFxAssetNextCall("core")
+            Wait(3000)
         end
     end
 end)
 
-function maleProgressBar()
-    if shower == true then
-        QBCore.Functions.Progressbar("shower", "Taking a shower", 9000, false, true, {
-            disableMovement = true,
-            disableCarMovement = true,
-            disableMouse = false,
-            disableCombat = true,
-        }, {
-            animDict = "anim@mp_yacht@shower@male@",
-            anim = "male_shower_enter_into_idle",
-            flags = 16,
-        }, {}, {}, function() -- Done
-            FreezeEntityPosition((PlayerPedId()), false)
-            ClearPedTasksImmediately(PlayerPedId()) 
-            StopParticleFxLooped(particles, 0) 
-            TriggerServerEvent('hud:server:RelieveStress', 25)
-            shower = false
-        end, function() -- Cancel
-            FreezeEntityPosition((PlayerPedId()), false)
-            ClearPedTasksImmediately(PlayerPedId()) 
-            StopParticleFxLooped(particles, 0) 
-            shower = false
-        end)
+function ProgressBar(sex)
+    local animDict = sex == "male" and "anim@mp_yacht@shower@male@" or "anim@mp_yacht@shower@female@"
+    local anim = sex == "male" and "male_shower_enter_into_idle" or "shower_enter_into_idle"
+    if showering then
+        if Config.Framework == 'QB' then
+            QBCore.Functions.Progressbar("shower", "Taking a shower", 9000, false, true, {
+                disableMovement = true,
+                disableCarMovement = true,
+                disableMouse = false,
+                disableCombat = true,
+            }, {
+                animDict = animDict,
+                anim = anim,
+                flags = 16,
+            }, {}, {}, function() -- Done
+                FreezeEntityPosition((PlayerPedId()), false)
+                ClearPedTasksImmediately(PlayerPedId())
+                StopParticleFxLooped(particles, false)
+                TriggerServerEvent('hud:server:RelieveStress', 25)
+                showering = false
+            end, function() -- Cancel
+                FreezeEntityPosition((PlayerPedId()), false)
+                ClearPedTasksImmediately(PlayerPedId())
+                StopParticleFxLooped(particles, false)
+                showering = false
+            end)
+        elseif Config.Framework == 'OX' then
+            if lib.progressBar({
+                duration = 9000,
+                label = 'Taking a shower',
+                useWhileDead = false,
+                canCancel = true,
+                disable = {
+                    move = true,
+                    car = true,
+                    combat = true
+                },
+                anim = {
+                    dict = animDict,
+                    clip = anim,
+                    flag = 16
+                }
+            }) then
+                FreezeEntityPosition((PlayerPedId()), false)
+                ClearPedTasksImmediately(PlayerPedId())
+                StopParticleFxLooped(particles, false)
+                TriggerServerEvent('hud:server:RelieveStress', 25)
+                showering = false
+            else
+                FreezeEntityPosition((PlayerPedId()), false)
+                ClearPedTasksImmediately(PlayerPedId())
+                StopParticleFxLooped(particles, false)
+                showering = false
+            end
+        end
     end
 end
-function femaleProgressBar()
-    if shower == true then
-        QBCore.Functions.Progressbar("shower", "Taking a shower", 9000, false, true, {
-            disableMovement = true,
-            disableCarMovement = true,
-            disableMouse = false,
-            disableCombat = true,
-        }, {
-            animDict = "anim@mp_yacht@shower@female@",
-            anim = "shower_enter_into_idle",
-            flags = 16,
-        }, {}, {}, function() -- Done
-            FreezeEntityPosition((PlayerPedId()), false)
-            ClearPedTasksImmediately(PlayerPedId()) 
-            StopParticleFxLooped(particles, 0) 
-            TriggerServerEvent('hud:server:RelieveStress', 25)
-            shower = false
-        end, function() -- Cancel
-            FreezeEntityPosition((PlayerPedId()), false)
-            ClearPedTasksImmediately(PlayerPedId()) 
-            StopParticleFxLooped(particles, 0) 
-            shower = false
-        end)
-    end
-end
+
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-AddEventHandler('onResourceStart', function(resource)
-   if resource == GetCurrentResourceName() then
-      print('Serrulata-Shower started')
-   end
+AddEventHandler('onResourceStart', function(resource) if resource ~= GetCurrentResourceName() then return end
+    print('Serrulata-Shower started')
 end)
-AddEventHandler('onResourceStop', function(resource)
-   if resource == GetCurrentResourceName() then
-      if Config.Framework == 'QB' then
-        exports['qb-target']:RemoveZone("sphere1")
+
+AddEventHandler('onResourceStop', function(resource) if resource ~= GetCurrentResourceName() then return end
+    if Config.Framework == 'QB' then
+        for k, v in pairs(targets) do
+            exports['qb-target']:RemoveZone(k)
+        end
         print('Serrulata-Shower stopped')
-      elseif Config.Framework == 'OX' then
+    elseif Config.Framework == 'OX' then
         print('Serrulata-Shower stopped')
-      end
-   end
+    end
 end)
--- config.lua
+++ config.lua
@@ -6,15 +6,6 @@ Config.Framework = 'QB' -- 'QB', 'OX'
 
 --// Target Zones \\--
 Config.Locations = {
-    [1] = {
-        coords = vector3(-767.26, 327.2, 170.64), 
-        radius = 1.0,
-        name = 'sphere1',
-    },
-    --[[ Example for adding more
-    [2] = {
-        coords = vector3(-720.22, 333.45, 30.62), 
-        radius = 1.0,
-        name = 'sphere2',
-    },]]
+    { coords = vector3(-767.26, 327.2, 170.64), radius = 1.0 },
+    -- { coords = vector3(-720.22, 333.45, 30.62),  radius = 1.0 }, -- Example for adding more
 }
